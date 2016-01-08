005fec770120093b00186aebe013acaf75e12525
jhorcicka <j.horcicka@gmc.net>
2016-01-08 14:06:07 +0100

    Spring Autowired.

---
+++ autowired/src/main/java/cz/hk/gmc/autowired/Person.java
@@ -0,0 +1,40 @@
+package cz.hk.gmc.autowired;
+
+public class Person {
+    private String name;
+    private String address;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(final String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(final int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
