850b253d9fda51f3428da99c0a2b8bdf7e6cb67e
jhorcicka <jakub.horcicka@humaninference.com>
2016-06-01 11:15:09 +0200

    SWING * ProgressBar: running in Thread.

--- swing/src/main/java/cz/hk/gmc/sandbox/ProgressBar.java
+++ swing/src/main/java/cz/hk/gmc/sandbox/ProgressBar.java
@@ -8,27 +8,65 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class ProgressBar extends JFrame {
+public class ProgressBar {
+    private ProgressBarFrame _frame;
+    private Thread _thread;
+
     public static void main(String[] args) {
-        ProgressBar frame = new ProgressBar();
-        frame.setVisible(true);
+        try {
+            ProgressBar bar = new ProgressBar();
+            bar.show();
+            Thread.sleep(3000);
+            bar.hide();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public ProgressBar() {
-        URL imageUrl = getClass().getResource("/loading.gif");
-        ImageIcon image = new ImageIcon(imageUrl);
-        JLabel label = new JLabel("", image, JLabel.CENTER);
+    private class ProgressBarFrame extends JFrame implements Runnable {
+        private static final String PATH = "/loading.gif";
+        private static final int WIDTH = 208;
+        private static final int HEIGHT = 13;
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        final Toolkit toolkit = Toolkit.getDefaultToolkit();
-        final Dimension screenSize = toolkit.getScreenSize();
-        int iconWidth = 208;
-        int iconHeight = 13;
-        final int x = (screenSize.width - this.getWidth()) / 2 - iconWidth / 2;
-        final int y = (screenSize.height - this.getHeight()) / 2 - iconHeight / 2;
-        setBounds(x, y, iconWidth, iconHeight);
+        private ProgressBarFrame() {
+            setUndecorated(true);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        add(label);
-        setUndecorated(true);
+            final URL imageUrl = getClass().getResource(PATH);
+            final ImageIcon image = new ImageIcon(imageUrl);
+            final JLabel label = new JLabel("", image, JLabel.CENTER);
+            add(label);
+
+            final Toolkit toolkit = Toolkit.getDefaultToolkit();
+            final Dimension screenSize = toolkit.getScreenSize();
+            final int x = (screenSize.width - getWidth()) / 2 - WIDTH / 2;
+            final int y = (screenSize.height - getHeight()) / 2 - HEIGHT / 2;
+            setBounds(x, y, WIDTH, HEIGHT);
+        }
+
+        public void run() {
+            setAlwaysOnTop(true);
+            setVisible(true);
+        }
+
+        private void terminate() {
+            setVisible(false);
+            dispose();
+        }
+    }
+
+    public void show() {
+        _frame = new ProgressBarFrame();
+        _thread = new Thread(_frame);
+        _thread.start();
+    }
+
+    public void hide() {
+        try {
+            _frame.terminate();
+            _thread.interrupt();
+            _thread.join();
+        } catch (InterruptedException e) {
+        }
     }
 }
