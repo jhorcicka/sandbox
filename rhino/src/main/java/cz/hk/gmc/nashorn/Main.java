35275a046af199416286846e62f7bdcb64e58b98
jhorcicka <jakub.horcicka@humaninference.com>
2016-09-19 13:11:32 +0200

    RHINO: variant with nashorn.

---
+++ rhino/src/main/java/cz/hk/gmc/nashorn/Main.java
@@ -0,0 +1,31 @@
+package cz.hk.gmc.nashorn;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    
+    public static void main(String args[]) {
+        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+        ScriptEngine engine = scriptEngineManager.getEngineByName("nashorn");
+        
+        //logger.debug("java: a={},b={}", 42, 'B');
+        logger.debug("java: a={},b={},c={}", 42, 'B', new String[] { "1", "2" });
+        
+        try {
+            //final String js = "logger.debug('javascript: a={},b={}', 42, 'B')";
+            final String js = "logger.debug('javascript: a={},b={},c={}', 42, 'B', ['1','2'])";
+
+            engine.getContext().setAttribute("logger", logger, ScriptContext.GLOBAL_SCOPE);
+            final Object result = engine.eval(js);
+        } catch(ScriptException e) {
+            e.printStackTrace();
+        }
+    }
+}
