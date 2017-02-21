a963d2be532af225aa2c4f65754a8de47531973c
jakub horcicka <jakub.horcicka@humaninference.com>
2017-02-21 10:42:35 +0100

    SWING: Java8.

--- various/src/main/java/cz/hk/gmc/various/Java8.java
+++ various/src/main/java/cz/hk/gmc/various/Java8.java
@@ -1,15 +1,31 @@
 package cz.hk.gmc.various;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 class Java8 {
     public static void main(String[] args) {
         final Java8 java8 = new Java8();
-        java8.put("hello");
-        //java8.put(null);
+        java8.streams();
+
+        java8.optional("hello");
+        //java8.optional(null);
     }
 
-    public void put(final String string) {
+    public void streams() {
+        final List<String> list = new ArrayList<>();
+        list.add("aaa1");
+        list.add("bbb1");
+        list.add("ccc1");
+        list.add("aaa2");
+        list.add("bbb2");
+        list.add("ccc2");
+
+        list.stream().filter((s) -> s.startsWith("b")).forEach(System.out::println);
+    }
+
+    public void optional(final String string) {
         Optional<String> optional = Optional.of(string);
         System.out.println("Input=" + string);
         System.out.println("IsPresent=" + optional.isPresent());
