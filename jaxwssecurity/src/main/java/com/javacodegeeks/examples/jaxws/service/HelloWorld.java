3c94d7e9959a02fa79d68043284a6850a7bc0a4f
jakub <j.horcicka@quadient.com>
2018-11-14 14:12:06 +0100

    Jaxwssecurity: Working solution, with SOAP request.

--- jaxwssecurity/src/main/java/com/javacodegeeks/examples/jaxws/service/HelloWorld.java
+++ jaxwssecurity/src/main/java/com/javacodegeeks/examples/jaxws/service/HelloWorld.java
@@ -1,12 +1,27 @@
 package com.javacodegeeks.examples.jaxws.service;
 
-import java.io.FileWriter;
-
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 /**
  * https://examples.javacodegeeks.com/enterprise-java/jws/jax-ws-client-basic-authentication-example/#1.4_subsec
+ * 
+ * SOAP REQUEST: 
+ <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.jaxws.examples.javacodegeeks.com/">
+     <soapenv:Header> 
+         <wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" soapenv:mustUnderstand="1"> 
+             <wsse:UsernameToken xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" wsu:Id="UsernameToken-1"> 
+                 <wsse:Username>my-username</wsse:Username> 
+                 <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">my-password</wsse:Password> 
+             </wsse:UsernameToken> 
+         </wsse:Security> 
+     </soapenv:Header>
+     <soapenv:Body>
+         <ser:sayHello>
+             <arg0>john</arg0>
+         </ser:sayHello>
+     </soapenv:Body>
+ </soapenv:Envelope>
  */
 @WebService
 public class HelloWorld {
@@ -15,16 +30,6 @@ public class HelloWorld {
 
     @WebMethod
     public String sayHello(String name) {
-        log("in sayHello: name=" + name);
         return "hello " + name;
-    }
-
-    private void log(String message) {
-        try {
-            FileWriter fileWriter = new FileWriter("/tmp/mytodo2.txt");
-            fileWriter.write(message);
-            fileWriter.close();
-        } catch (Exception e) {
-        }
     }
 }
--- jaxwssecurity/src/main/java/com/javacodegeeks/examples/jaxws/service/PasswordCallback.java
+++ jaxwssecurity/src/main/java/com/javacodegeeks/examples/jaxws/service/PasswordCallback.java
@@ -1,47 +1,27 @@
 package com.javacodegeeks.examples.jaxws.service;
 
-import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
-import org.apache.ws.security.WSPasswordCallback;
+import org.apache.wss4j.common.ext.WSPasswordCallback;
 
 public class PasswordCallback implements CallbackHandler {
-    public PasswordCallback() {
-        log("PasswordCallback constructor");
-    }
-    
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-        String debug = "MYTODO: handle";
         for (int i = 0; i < callbacks.length; i++) {
-            WSPasswordCallback pc = (WSPasswordCallback) callbacks[i];
+            final WSPasswordCallback callback = (WSPasswordCallback) callbacks[i];
 
-            debug += "MYTODO: pc.getUsage=" + pc.getUsage();
-            debug += "MYTODO: pc.getIdentifier=" + pc.getIdentifier();
-            if (pc.getUsage() == WSPasswordCallback.USERNAME_TOKEN) {
+            if (callback.getUsage() == WSPasswordCallback.USERNAME_TOKEN) {
                 //You must set a password for the user, WSS4J would compare
                 //the password with the password sent by client, if they match
                 //message will be processed. Any mismatch in password will result in a SOAP Fault.
 
-                if (pc.getIdentifier().equalsIgnoreCase("dxmanda")) {
-                    pc.setPassword("my-pass");
+                if (callback.getIdentifier().equalsIgnoreCase("my-username")) {
+                    callback.setPassword("my-password");
                 }
             }
-            pc.setPassword("my-pass");
-        }
-
-        log(debug);
-    }
-    
-    private void log(String message) {
-        try {
-            FileWriter fileWriter = new FileWriter("/tmp/mytodo.txt");
-            fileWriter.write(message);
-            fileWriter.close();
-        } catch (Exception e) {
         }
     }
 }
