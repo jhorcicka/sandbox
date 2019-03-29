e12fd7cf6411b5e3049f54d1edc1d052205287f5
jakub <j.horcicka@quadient.com>
2019-03-29 10:38:13 +0100

    Jersey: POST.

--- jersey/src/main/java/cz/hk/gmc/jersey/ClientDemo.java
+++ jersey/src/main/java/cz/hk/gmc/jersey/ClientDemo.java
@@ -2,6 +2,7 @@ package cz.hk.gmc.jersey;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
@@ -12,6 +13,26 @@ import org.glassfish.jersey.filter.LoggingFilter;
 
 public class ClientDemo {
     public static void main(String[] args) {
+        get();
+        post();
+    }
+    
+    private static void post() {
+        Client client = ClientBuilder.newClient(new ClientConfig().register(LoggingFilter.class));
+        WebTarget webTarget =
+                client.target("http://localhost:8080/jersey-1.0-SNAPSHOT/rest/message").path("employees");
+        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_XML);
+        Employee employee = new Employee();
+        employee.setId(42);
+        employee.setName("John Doe");
+        
+        Response response = invocationBuilder.post(Entity.entity(employee, MediaType.APPLICATION_XML));
+
+        System.out.println(response.getStatus());
+        System.out.println(response.readEntity(String.class));
+    }   
+    
+    private static void get() {
         Client client = ClientBuilder.newClient(new ClientConfig().register(LoggingFilter.class));
         WebTarget webTarget =
                 client.target("http://localhost:8080/jersey-1.0-SNAPSHOT/rest/message").path("employees").path("1");
