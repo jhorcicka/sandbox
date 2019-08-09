d77c941f0627ab37b05e00a9ebf5d1c7851b48b3
jakub <j.horcicka@quadient.com>
2019-08-09 11:12:48 +0200

    JIRA

--- jira/src/main/java/nl/hi/kuba/jira/JiraClient.java
+++ jira/src/main/java/nl/hi/kuba/jira/JiraClient.java
@@ -1,4 +1,60 @@
 package nl.hi.kuba.jira;
 
-public class JiraClient {
+import java.net.URI;
+import java.util.concurrent.ExecutionException;
+
+import com.atlassian.jira.rest.client.api.IssueRestClient;
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.SearchResult;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import com.atlassian.util.concurrent.Promise;
+
+class JiraClient {
+    private String username;
+    private String password;
+    private String jiraUrl;
+    private JiraRestClient restClient;
+
+    JiraClient(String username, String password, String jiraUrl) {
+        this.username = username;
+        this.password = password;
+        this.jiraUrl = jiraUrl;
+        this.restClient = getJiraRestClient();
+    }
+
+    private JiraRestClient getJiraRestClient() {
+        return new AsynchronousJiraRestClientFactory()
+                .createWithBasicHttpAuthentication(getJiraUri(), this.username, this.password);
+    }
+
+    private URI getJiraUri() {
+        return URI.create(this.jiraUrl);
+    }
+
+    Issue getIssue(final String issueId) throws ExecutionException, InterruptedException {
+        IssueRestClient issueClient = restClient.getIssueClient();
+        final Promise<Issue> issue = issueClient.getIssue(issueId);
+        return issue.get();
+    }
+
+    String getSprint(final String sprintId) throws ExecutionException, InterruptedException {
+        final SearchResult searchResult = restClient.getSearchClient().searchJql(
+                "(project = DC or project = DI) and sprint in openSprints() and (issuetype = bug or issuetype = story)")
+                .get();
+        for (Issue issue : searchResult.getIssues()) {
+            System.err.println("issue: " + issue.getKey() + ": " + issue.getSummary());
+        }
+        
+        /*
+        ProjectRestClient projectRestClient = restClient.getProjectClient();
+        final Promise<Project> projectPromise = projectRestClient.getProject("DC");
+        final Project project = projectPromise.get();
+        project.
+        for (BasicComponent bc : project.getComponents()) {
+            System.err.println("MYTODO: " + bc.getName());
+        }
+        */
+        return "todo";
+    }
 }
--- jira/src/main/java/nl/hi/kuba/jira/Main.java
+++ jira/src/main/java/nl/hi/kuba/jira/Main.java
@@ -1,7 +1,41 @@
 package nl.hi.kuba.jira;
 
+import com.atlassian.jira.rest.client.api.domain.Issue;
+
 public class Main {
+    private static String USERNAME = "";
+    private static String PASSWORD = "";
+    private static String URL = "";
+    
     public static void main(String[] args) {
-        System.err.println("MYTODO: ");
+        try {
+            final JiraClient client = new JiraClient(USERNAME, PASSWORD, URL);
+            final String sprint = client.getSprint("Papa");
+            System.err.println("MYTODO: " + sprint);
+            /*
+            sprint info
+            - number of issues
+            - commitment issues
+            - added issues
+            - removed issues
+            */
+
+            final Issue issue = client.getIssue("DI-104");
+            /*
+            issue info needed
+            - v ID
+            - v epic name
+            - type (bug/story)
+            - status
+             */
+            if (issue != null) {
+                System.err.println(
+                        "MYTODO:\n" + "id=" + issue.getKey() + "\n" + "type=" + issue.getIssueType().getName() + "\n"
+                                + "epic=" + issue.getFieldByName("Epic Link").getValue() + "\n" + "status=" + issue
+                                .getStatus().getName() + "\n");
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
     }
 }
