8469665afc98f34ae885a8fd435e6124823d8438
jakub <j.horcicka@quadient.com>
2020-12-24 15:29:30 +0100

    Streams.

--- java-8-streams/src/main/java/nl/hi/kuba/streams/StreamsDemo.java
+++ java-8-streams/src/main/java/nl/hi/kuba/streams/StreamsDemo.java
@@ -1,41 +1,72 @@
 package nl.hi.kuba.streams;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
 public class StreamsDemo {
+    private static final List<Integer> NUMBERS = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+
     public static void main(String[] args) {
         //sequential();
         //forEach();
-        aggregateFunctions();
+        //aggregateFunctions();
+        //collecting();
+        //statistics();
+    }
+
+    private static void statistics() {
+        final IntSummaryStatistics statistics = NUMBERS.stream().mapToInt((element) -> element).summaryStatistics();
+
+        System.out.println("max=" + statistics.getMax());
+        System.out.println("min=" + statistics.getMin());
+        System.out.println("sum=" + statistics.getSum());
+        System.out.println("average=" + statistics.getAverage());
     }
 
     private static void aggregateFunctions() {
-        final List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
-
         System.out.println("distinct");
-        numbers.stream().distinct().forEach(System.out::println);
+        NUMBERS.stream().distinct().forEach(System.out::println);
         System.out.println("map");
-        numbers.stream().map(i -> i * i).forEach(System.out::println);
+        NUMBERS.stream().map(i -> i * i).forEach(System.out::println);
         System.out.println("filter");
-        numbers.stream().filter(i -> i > 3).forEach(System.out::println);
+        NUMBERS.stream().filter(i -> i > 3).forEach(System.out::println);
         System.out.println("limit");
-        numbers.stream().limit(2).forEach(System.out::println);
+        NUMBERS.stream().limit(2).forEach(System.out::println);
         System.out.println("sorted ASC");
-        numbers.stream().sorted().forEach(System.out::println);
+        NUMBERS.stream().sorted().forEach(System.out::println);
         System.out.println("sorted DESC");
-        numbers.stream().sorted((i1, i2) -> Integer.compare(i2, i1)).forEach(System.out::println);
-
-        // limit
-        // reduce
-        // find
-        // match
+        NUMBERS.stream().sorted((i1, i2) -> Integer.compare(i2, i1)).forEach(System.out::println);
+        System.out.println("reduce");
+        final int total = NUMBERS.stream().reduce(0, (subtotal, element) -> subtotal + element);
+        System.out.println("total=" + total);
+        System.out.println("find");
+        final int any = NUMBERS.stream().findAny().get();
+        System.out.println("any=" + any);
+        final int first = NUMBERS.stream().findFirst().get();
+        System.out.println("first=" + first);
+        final boolean anyMatch = NUMBERS.stream().anyMatch(element -> element > 2);
+        System.out.println("anyMatch=" + anyMatch);
+        final boolean allMatch = NUMBERS.stream().allMatch(element -> element > 2);
+        System.out.println("allMatch=" + allMatch);
     }
 
     private static void collecting() {
-        // collectors
+        // list
+        NUMBERS.stream().filter(element -> element > 2).collect(Collectors.toList()).forEach(System.out::println);
+        // array
+        final Integer[] biggerThan2 = NUMBERS.stream().filter(element -> element > 2).toArray(Integer[]::new);
+        System.out.println("biggerThan2: ");
+        Arrays.stream(biggerThan2).forEach(System.out::println);
+        // joining
+        final String joinedString = NUMBERS
+                .stream()
+                .filter(element -> element > 2)
+                .map(element -> element.toString())
+                .collect(Collectors.joining("x"));
+        System.out.println("JoinedString=" + joinedString);
     }
 
     private static void forEach() {
